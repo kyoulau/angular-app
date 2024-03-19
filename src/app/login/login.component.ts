c851451ade73e132fef0f90d90fa4b2862cd9f32
Laura da Costa dos Santos <laura.costa@pucpr.edu.br>
2024-03-18 21:27:44 -0300

    criacao validacao login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  showErrorMessages = false;
+  showSuccessMessages = false;
+
+  formGroup = new FormGroup({
+    email: new FormControl('',
+    [Validators.pattern(/[^\s]*@[a-z0-9.-]*/i), Validators.required]),
+    senha: new FormControl('', Validators.minLength(5))
+  })
+
+salvar(){
+    console.log(this.formGroup.controls.email.invalid);
+    console.log(this.formGroup.controls.senha.touched);
+    
+    if (this.formGroup.invalid) { 
+      console.log('Formulário inválido');
+      this.formGroup.markAllAsTouched();
+      this.showErrorMessages = true;
+      return;
+    }
+
+    console.log('Formulário válido');
+    this.showSuccessMessages = true;
+
+}
 }
